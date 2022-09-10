f9e654b58fe291ec837c4097d032ffb245c860d3
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-09-10 20:48:46 +0530

    Create SendSensorData.cpp

---
+++ SendSensorData.cpp
@@ -0,0 +1,18 @@
+#include "SendSensorData.hpp"
+
+bool transferData()
+{
+    bool sendStatus = false;
+    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE , MIN_TEMPERATURE_VALUE , TOTAL_READINGS);
+    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE , MIN_CHARGE_VALUE , TOTAL_READINGS);
+    if((tempData.size() == TOTAL_READINGS) && (chargeData.size() == TOTAL_READINGS))
+    {
+        for(int count = 0; count < TOTAL_READINGS; count ++)
+        {
+            cout << tempData[count]<< "," << chargeData[count] <<endl;
+        }
+        sendStatus = true;
+    }
+    return sendStatus;
+
+}
