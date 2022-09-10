2ac012949fe218afde188704122dae3a268173da
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-09-10 20:56:37 +0530

    Create Test_Sender.cpp

---
+++ Test_Sender.cpp
@@ -0,0 +1,23 @@
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
+#include <vector>
+#include <iostream>
+#include "TEMPERATURE.hpp"
+#include "CHARGE.hpp"
+#include "SendSensorData.hpp"
+
+using namespace std;
+
+TEST_CASE("To check if temperature values are generated correctly") 
+{
+    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE , MIN_TEMPERATURE_VALUE , TOTAL_READINGS);
+    int tSize = temperatureData.size();
+    REQUIRE(tSize == TOTAL_READINGS);
+}
+
+TEST_CASE("To check if charge values are generated correctly") 
+{
+    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE , MIN_CHARGE_VALUE , TOTAL_READINGS);
+    int cSize = chargeData.size();
+    REQUIRE(cSize == TOTAL_READINGS);
+}
