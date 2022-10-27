ecdae1a016f5d3218931befc6d6bea569dfe919d
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-10-27 11:31:11 +0530

    Update Test_Sender.cpp

--- Test_Sender.cpp
+++ Test_Sender.cpp
@@ -8,16 +8,43 @@
 
 using namespace std;
 
-TEST_CASE("To check if temperature values are generated correctly") 
+TEST_CASE("To check if Negetive scenario is working properly")
+
 {
-    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE , MIN_TEMPERATURE_VALUE , TOTAL_READINGS);
+    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE, MIN_TEMPERATURE_VALUE, 0);
+    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE, MIN_CHARGE_VALUE, 0);
     int tSize = tempData.size();
-    REQUIRE(tSize == TOTAL_READINGS);
+    int cSize = chargeData.size();
+    if (transferData(tempData, chargeData))
+    {
+        REQUIRE(tSize == TOTAL_READINGS);
+        REQUIRE(cSize == TOTAL_READINGS);
+    }
+    else
+    {
+        REQUIRE(tSize == 0);
+        REQUIRE(cSize == 0);
+    }
 }
 
-TEST_CASE("To check if charge values are generated correctly") 
+TEST_CASE("To check if temperature and charge values are generated correctlyY")
+
 {
-    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE , MIN_CHARGE_VALUE , TOTAL_READINGS);
+    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE, MIN_TEMPERATURE_VALUE, TOTAL_READINGS);
+    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE, MIN_CHARGE_VALUE, TOTAL_READINGS);
+    int tSize = tempData.size();
     int cSize = chargeData.size();
-    REQUIRE(cSize == TOTAL_READINGS);
+    if (transferData(tempData, chargeData))
+    {
+        REQUIRE(tSize == TOTAL_READINGS);
+        REQUIRE(cSize == TOTAL_READINGS);
+    }
+    else
+    {
+        REQUIRE(tSize == 0);
+        REQUIRE(cSize == 0);
+        __assert_fail("tEST failed", "Test_Sender.cpp", 24, "error");
+
+    }
+
 }
