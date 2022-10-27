03d20c480ac58f889b28ae8bb027ede0c02f8cbe
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-10-27 11:38:50 +0530

    Update Test_Sender.cpp

--- Test_Sender.cpp
+++ Test_Sender.cpp
@@ -7,12 +7,10 @@
 #include "SendSensorData.hpp"
 
 using namespace std;
-
-TEST_CASE("To check if Negetive scenario is working properly")
-
-{
-    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE, MIN_TEMPERATURE_VALUE, 0);
-    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE, MIN_CHARGE_VALUE, 0);
+int SendSendersize(int total_reading);
+{ 
+    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE, MIN_TEMPERATURE_VALUE, total_reading);
+    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE, MIN_CHARGE_VALUE, total_reading);
     int tSize = tempData.size();
     int cSize = chargeData.size();
     if (transferData(tempData, chargeData))
@@ -26,25 +24,15 @@ TEST_CASE("To check if Negetive scenario is working properly")
         REQUIRE(cSize == 0);
     }
 }
+TEST_CASE("To check if Negetive scenario is working properly")
+
+{
+   SendSendersize(0);
+}
 
 TEST_CASE("To check if temperature and charge values are generated correctlyY")
 
 {
-    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE, MIN_TEMPERATURE_VALUE, TOTAL_READINGS);
-    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE, MIN_CHARGE_VALUE, TOTAL_READINGS);
-    int tSize = tempData.size();
-    int cSize = chargeData.size();
-    if (transferData(tempData, chargeData))
-    {
-        REQUIRE(tSize == TOTAL_READINGS);
-        REQUIRE(cSize == TOTAL_READINGS);
-    }
-    else
-    {
-        REQUIRE(tSize == 0);
-        REQUIRE(cSize == 0);
-        __assert_fail("tEST failed", "Test_Sender.cpp", 24, "error");
-
-    }
+    SendSendersize(TOTAL_READINGS);
 
 }
