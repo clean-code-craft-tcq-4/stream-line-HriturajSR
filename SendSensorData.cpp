470d9e04fee596cf88bda52a4e5cd3646c37e2a5
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-10-27 11:25:08 +0530

    Update SendSensorData.cpp

--- SendSensorData.cpp
+++ SendSensorData.cpp
@@ -1,11 +1,8 @@
 #include "SendSensorData.hpp"
 
-bool transferData()
+bool transferData(std::vector<int>tempData,std::vector<int>chargeData)
 {
     bool sendStatus = false;
-    vector<int> tempData = generateTemperatureValues(MAX_TEMPERATURE_VALUE , MIN_TEMPERATURE_VALUE , TOTAL_READINGS);
-    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE , MIN_CHARGE_VALUE , TOTAL_READINGS);
-    std::cout<<tempData[0]<<","<< chargeData[0]<<std::endl;
     if((tempData.size() == TOTAL_READINGS) && (chargeData.size() == TOTAL_READINGS))
     {
         for(int count = 0; count < TOTAL_READINGS; count ++)
