3e80c99a071d3467c1d0d733a3d99607b70a361c
Rihan Malik <rihannb@yahoo.in>
2022-10-31 19:54:58 +0530

    Receiver initial commit

--- bmsReceiver.c
+++ bmsReceiver.c
@@ -1,21 +1,56 @@
 #include <stdio.h>
-float Temp[50];
-float ChargingRate[50];
+#include "bmsReceiver.h"
+
+struct BMSParameters bmsParameters;
+struct BMSProcessedParameters processedParameters;
 
 int ReadParameters(void)
 {
- for (int i = 0; i< 50;i++)
- { 
-   scanf("%f,%f\n", &Temp[i], &ChargingRate[i]);
- }
-}
-
-int displayReadData(void)
-{
     for (int i = 0; i< 50;i++)
     { 
-        printf("Temp-%f,CR-%f\n", Temp[i], ChargingRate[i]);
+        scanf("%f,%f\n", &bmsParameters.temperature[i], &bmsParameters.chargeRate[i]);
+
+        processedParameters.maxTemp = calculateMaxValue (bmsParameters.temperature[i], processedParameters.maxTemp);
+        processedParameters.minTemp = calculateMinValue (bmsParameters.temperature[i], processedParameters.minTemp);
+        
+        processedParameters.maxChargeRate = calculateMaxValue (bmsParameters.chargeRate[i], processedParameters.maxChargeRate);
+        processedParameters.minChargeRate = calculateMinValue (bmsParameters.chargeRate[i], processedParameters.minChargeRate);
     }
+}
+
+int 
+displayReadData(void)
+{
+    for (int i = 0; i< 50; i++)
+    { 
+        printf("Temp-%f,CR-%f\n", bmsParameters.temperature[i], bmsParameters.chargeRate[i]);
+    }
+
+    printf ("MaxTemp = %f\t MinTemp= %f\t MaxCR = %f\t MinCR =%f",
+        processedParameters.maxTemp, processedParameters.minTemp,
+        processedParameters.maxChargeRate, processedParameters.minChargeRate);
+}
+
+float
+calculateMaxValue(float currentValue, float maxValue)
+{
+    if (currentValue != 0 && currentValue =< maxValue)
+    {
+        return maxValue;
+    }
+
+    return currentValue;
+}
+
+float
+calculateMinValue(float currentValue, float minValue)
+{
+    if (currentValue != 0 && currentValue >= minValue)
+    {
+        return minValue;
+    }
+
+    return currentValue;
 }
 
 int main()
--- bmsReceiver.h
+++ bmsReceiver.h
@@ -1,1 +1,14 @@
-//Test
+struct BMSProcessedParameters{
+    float minTemp;
+    float maxTemp;
+
+    float minChargeRate;
+    float maxChargeRate;
+
+    float smaChargeRate;
+    float smaTemp;
+};
+struct BMSParameters{
+    float temperature[50];
+    float chargeRate[50];
+};
