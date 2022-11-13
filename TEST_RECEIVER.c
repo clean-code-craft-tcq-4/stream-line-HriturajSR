c0fd7bc400df52dd8c52f962108b928a612adfb6
Rihan Malik <rihannb@yahoo.in>
2022-11-13 16:37:02 +0530

    debug

--- TEST_RECEIVER.c
+++ TEST_RECEIVER.c
@@ -26,15 +26,21 @@ TEST_PROCESS_DATA (void)
 void
 TEST_MAX_AND_MIN (void)
 {
+    printf("TEST max n min : %f - %f - %f -%f\n",testprocessedParameters.maxChargeRate,testprocessedParameters.minChargeRate,
+                testprocessedParameters.maxTemp, testprocessedParameters.minTemp);
     assert (abs(testprocessedParameters.maxChargeRate) == 79);
     assert (abs(testprocessedParameters.minChargeRate) == 39);
     assert (abs(testprocessedParameters.maxTemp) == 40);
     assert (abs(testprocessedParameters.minTemp) == 6);
+    printf("***********Max n min test passed***********\n");
 }
 
 void
 TEST_SMA(void)
 {
+    for (int i = 0; i <=5 ; i++)
+        printf("TEST: %f - %f abs %f - %f\n",testprocessedParameters.smaChargeRate[i],testprocessedParameters.smaTemp[i],
+                        abs(testprocessedParameters.smaChargeRate[i]),abs(testprocessedParameters.smaTemp[i]));
     assert (abs(testprocessedParameters.smaChargeRate[0]) == abs(67.8));
     assert (abs(testprocessedParameters.smaChargeRate[1]) == abs(66.4));
     assert (abs(testprocessedParameters.smaChargeRate[2]) == abs(59.6));
@@ -48,6 +54,7 @@ TEST_SMA(void)
     assert (abs(testprocessedParameters.smaTemp[3]) == abs(14.2));
     assert (abs(testprocessedParameters.smaTemp[4]) == abs(20.8));
     assert (abs(testprocessedParameters.smaTemp[5]) == abs(20.2));
+    printf("***********SMA test passed***********\n");
 }
 
 void
